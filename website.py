5db9fe6a8c59a750c3316d3babe4d7c42b6f0cfb
Jesse Briggs <j@pc>
2013-05-31 23:15:23 -0400

    Nearly finished version of class Website

---
+++ website.py
@@ -0,0 +1,60 @@
+import time
+import requests
+
+class Website:
+	def __init__(self, url, interval, suckers):
+		self.url = url
+		self.interval = interval
+		self.time_for_next = time.time()
+		self.last_response = 0
+		self.suckers = suckers
+		self.status = 0
+
+	def get(self):
+		r = requests.get(self.url)
+		self.last_response = r.status_code
+		return r.status_code
+
+	def clear(self):
+		if self.status > 2:
+			self.alert_up()
+		self.status = 0
+
+	def check(self):
+		def get_interval():
+			if self.status == 1:
+				return 5
+			else:
+				return self.interval
+
+		r = self.get()
+		if r != 200:
+			self.status += 1
+			if self.status == 2:
+				self.alert()
+			else:
+				self.report()
+		else:
+			self.clear()
+			self.report()
+
+		self.set_next_time(get_interval())
+
+	def set_next_time(self, interval):
+		self.time_for_next = time.time() + interval
+
+	def alert(self):
+		for sucker in self.suckers:
+			sucker.sms(self.url, self.last_response)
+
+	def alert_up(self):
+		for sucker in self.suckers:
+			sucker.sms(self.url, self.last_response)
+
+	def report(self):
+		print self.url + ": " + str(self.last_response)
+
+	def run(self):
+		if time.time() > self.time_for_next:
+			self.check()
+			print self.url + ": next check in " + str(self.time_for_next - time.time()) + " seconds"
