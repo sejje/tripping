c8114bffe3b99043f06fea2105c74cca98fa3685
Jesse Briggs <j@pc>
2013-06-15 17:52:28 -0400

    Add email functionality

--- sucker.py
+++ sucker.py
@@ -6,3 +6,13 @@ class Sucker:
 
 	def sms(self, url, response, message):
 		print "SMS to " + self.name + ": " + message
+
+	def email(self, url, response, message):
+		#format like "Down: http://sejje.net (404)"
+		msg = "\n" + message + ": " + url + "(" + response + ")"
+		server = smtplib.SMTP(email_server)
+		server.starttls()
+		server.login(email_address, email_password)
+		server.sendmail(email_address, self.email, msg)
+		print msg
+		server.close
